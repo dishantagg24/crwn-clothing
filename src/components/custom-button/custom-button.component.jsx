474e9ecda0d696699f1ec0df400d07df6189bd48
dishantaggarwal <dishantaggarwal24@gmail.com>
2021-09-11 19:39:11 +0530

    added signin and button components

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './custom-button.styles.scss'
+
+const CustomButton = ({children, ...otherProps}) => (
+    <div>
+        <button className='custom-button' {...otherProps}>
+            {children}
+        </button>
+    </div>
+)
+
+export default CustomButton;
