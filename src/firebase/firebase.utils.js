27787777c47736438f483503357980e6aa90cc26
dishantaggarwal <dishantaggarwal24@gmail.com>
2021-09-13 16:21:04 +0530

    integrated firebase, added sign-in page and buttons

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const config = {
+    apiKey: "AIzaSyB90jQs665prkSnu9rUsga14KqWjZu_n-Y",
+    authDomain: "crwn-db-2ac46.firebaseapp.com",
+    projectId: "crwn-db-2ac46",
+    storageBucket: "crwn-db-2ac46.appspot.com",
+    messagingSenderId: "384788236310",
+    appId: "1:384788236310:web:1d9e1f8bef0fbff8ce0d37",
+    measurementId: "G-VS83CCZH61"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
